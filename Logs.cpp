a56f0ce5f5f97dff47fb261a8bb5ec77bcaa6b6a
Simon <u22490729@tuks.co.za>
2023-10-25 16:21:46 +0200

    Added file headers for Logs.cpp and Logs.h

--- Logs.cpp
+++ Logs.cpp
@@ -1,5 +1,13 @@
+//===-- COS214_Project/Logs.cpp - Logs class implementation ------*- C++ -*-===//
 //
-// Created by maili on 2023/10/25.
+// COS 214 Project.
 //
+//===----------------------------------------------------------------------===/////
+/// \file
+/// This file contains the implementation of the Logs class. Logs serves as a
+/// caretaker in the memento design pattern. It is used to store temporary
+/// SavedLogs and to save and load SavedLogs to disk.
+///
+//===----------------------------------------------------------------------===//
 
 #include "Logs.h"
