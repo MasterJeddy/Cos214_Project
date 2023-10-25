7542ebad9481a57ddb846bc4cd81d6b431d04474
Simon <u22490729@tuks.co.za>
2023-10-25 16:16:35 +0200

    Added file headers for Load.cpp, Load.h, Save.cpp ,Save.h

--- Load.h
+++ Load.h
@@ -1,6 +1,15 @@
+//===-- COS214_Project/Load.h - Load class definition ------*- C++ -*-===//
 //
-// Created by maili on 2023/10/25.
+// COS 214 Project.
 //
+//===----------------------------------------------------------------------===/////
+/// \file
+/// This file contains the declaration of the Load class. Load serves as
+/// a concrete command in the command pattern. The load command either
+/// loads a temporary SavedLog from the Logs object or uses the Logs object
+/// to load a log stored on disk.
+///
+//===----------------------------------------------------------------------===//
 
 #ifndef COS214_PROJECT__LOAD_H
 #define COS214_PROJECT__LOAD_H
--- Save.cpp
+++ Save.cpp
@@ -1,5 +1,13 @@
+//===-- COS214_Project/Save.cpp - Save class implementation ------*- C++ -*-===//
 //
-// Created by maili on 2023/10/25.
+// COS 214 Project.
 //
-
+//===----------------------------------------------------------------------===/////
+/// \file
+/// This file contains the implementation of the Save class. Save serves as
+/// a concrete command in the command pattern. The save command either
+/// creates a temporary SavedLog in the Logs object or use the Logs object
+/// to store a SavedLog on disk.
+///
+//===----------------------------------------------------------------------===//
 #include "Save.h"
