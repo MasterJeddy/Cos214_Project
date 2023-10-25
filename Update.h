f7c244b5879cf5b7863ff6a264a986a446397a08
Simon <u22490729@tuks.co.za>
2023-10-25 15:13:22 +0200

    Stubbed main and added intial skeleton classes. Got all inhertiance working. For practicallities sake put the more trivail commands in two files instead of 14.

---
+++ Update.h
@@ -0,0 +1,13 @@
+//
+// Created by maili on 2023/10/25.
+//
+
+#ifndef COS214_PROJECT__UPDATE_H
+#define COS214_PROJECT__UPDATE_H
+
+#include "UserCommand.h"
+class Update : public UserCommand{
+
+};
+
+#endif //COS214_PROJECT__UPDATE_H
--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 //
 // Created by maili on 2023/10/23.
 //
-
-int randomvalue(){
-  return 3;
-}
-
-int main() {
-
-  return 0;
+int main(){
+    return 0;
 }
