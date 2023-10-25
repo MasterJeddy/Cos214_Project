bcdbb5ecb23c6fbede64280288b1d6d47fb3f2cf
Simon <u22490729@tuks.co.za>
2023-10-25 16:07:04 +0200

    Added file headers for Commands.cpp and Commands.h

--- Commands.cpp
+++ Commands.cpp
@@ -1,5 +1,16 @@
+//===-- COS214_Project/Commands.cpp - Commands class implementation ------*- C++ -*-===//
 //
-// Created by maili on 2023/10/25.
+// COS 214 Project.
 //
+//===----------------------------------------------------------------------===/////
+/// \file
+/// This file contains the implementation of several concrete commands in the
+/// command pattern. Specifically HireMaitreD, BuyTable, ExpandFloor,
+/// HireWaiter, ExpandKitchen, HireChef and BuyStock. What these commands
+/// all have in common is that they will simple call corresponding functions
+/// on the KitchenController and FloorController facades. e.g. BuyStock will
+/// call the buyStock method on KitchenController.
+///
+//===----------------------------------------------------------------------===//
 
 #include "Commands.h"
--- Commands.h
+++ Commands.h
@@ -1,6 +1,17 @@
+//===-- COS214_Project/Commands.h - Commands class definition ------*- C++ -*-===//
 //
-// Created by maili on 2023/10/25.
+// COS 214 Project.
 //
+//===----------------------------------------------------------------------===/////
+/// \file
+/// This file contains the declaration of several concrete commands in the
+/// command pattern. Specifically HireMaitreD, BuyTable, ExpandFloor,
+/// HireWaiter, ExpandKitchen, HireChef and BuyStock. What these commands
+/// all have in common is that they will simple call corresponding functions
+/// on the KitchenController and FloorController facades. e.g. BuyStock will
+/// call the buyStock method on KitchenController.
+///
+//===----------------------------------------------------------------------===//
 
 #ifndef COS214_PROJECT__COMMANDS_H
 #define COS214_PROJECT__COMMANDS_H
