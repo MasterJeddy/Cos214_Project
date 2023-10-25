0522e1b0c6c1085023d7f54c68bbdee458a5f27c
Simon <u22490729@tuks.co.za>
2023-10-25 15:57:17 +0200

    Added file headers for CommandLog.cpp and CommandLog.h

--- CommandLog.cpp
+++ CommandLog.cpp
@@ -1,5 +1,15 @@
+//===-- COS214_Project/CommandLog.cpp - CommandLog class implementation ------*- C++ -*-===//
 //
-// Created by maili on 2023/10/25.
+// COS 214 Project.
 //
+//===----------------------------------------------------------------------===/////
+/// \file
+/// This file contains the implementation of the CommandLog class. CommandLog will
+/// serve as an originator in the memento pattern. As the EventPoll emits commands
+/// this class will log them. This log can be used by the EventPoll to be rolled
+/// back to. In practise this will mean all commands in the log will have to be
+/// rerun by the system at a much faster rate than usual play.
+///
+//===----------------------------------------------------------------------===//
 
 #include "CommandLog.h"
--- CommandLog.h
+++ CommandLog.h
@@ -1,6 +1,16 @@
+//===-- COS214_Project/CommandLog.h - CommandLog class definition ------*- C++ -*-===//
 //
-// Created by maili on 2023/10/25.
+// COS 214 Project.
 //
+//===----------------------------------------------------------------------===/////
+/// \file
+/// This file contains the declaration of the CommandLog class. CommandLog will
+/// serve as an originator in the memento pattern. As the EventPoll emits commands
+/// this class will log them. This log can be used by the EventPoll to be rolled
+/// back to. In practise this will mean all commands in the log will have to be
+/// rerun by the system at a much faster rate the usual play.
+///
+//===----------------------------------------------------------------------===//
 
 #ifndef COS214_PROJECT__COMMANDLOG_H
 #define COS214_PROJECT__COMMANDLOG_H
