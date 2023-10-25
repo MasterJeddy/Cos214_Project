24fcde06da3c55dbf41e74b2a8e75ed6452039d9
Simon <u22490729@tuks.co.za>
2023-10-25 15:26:27 +0200

    Added file headers for InputPoll.cpp and InputPoll.h

--- InputPoll.cpp
+++ InputPoll.cpp
@@ -1,5 +1,14 @@
+//===-- COS214_Project/InputPoll.cpp - InputPoll class implementation ------*- C++ -*-===//
 //
-// Created by maili on 2023/10/25.
+// COS 214 Project.
 //
+//===----------------------------------------------------------------------===/////
+/// \file
+/// This file contains the implementation of the InputPoll class. InputPoll will
+/// serve as an invoker in the command and the main class through which user
+/// can interact with the system. The entire system will be driven forward
+/// by the commands emitted by this class.
+///
+//===----------------------------------------------------------------------===//
 
 #include "InputPoll.h"
