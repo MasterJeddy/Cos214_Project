4fcb1ffbb39de88b0cee3ea85fa390cb31fcc3e3
Mihail <u22495292@tuks.co.za>
2023-10-26 20:47:16 +0200

    Documentation updated

--- Customer.cpp
+++ Customer.cpp
@@ -7,7 +7,7 @@
 /// \file
 /// This file contains the implementation of the Customer class functions as well as the methods declared
 /// by its parent class, more specifically the request() method where a customer can request to be seated
-/// at a table.
+/// at a table..
 ///
 //===----------------------------------------------------------------------===//
 
