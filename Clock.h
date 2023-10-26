e2d3cd1a1d10a72454f402707b0aee511c5b2aa7
Simon <u22490729@tuks.co.za>
2023-10-26 16:33:16 +0200

    Filled out relevent doxygen comments

--- Clock.h
+++ Clock.h
@@ -21,32 +21,50 @@ class Clock {
 private:
   std::map<std::string,int> timers;
 public:
+  /// \brief Instance function of the clock singleton
   ///
-  /// \return
+  /// A static function which returns the a reference to the singular clock object. Also creates the clock
+  /// if necessary.
+  ///
+  /// \return A reference to the singular clock instance
   static Clock& instance();
+  /// \breif Get the time associated with identification string.
   ///
-  /// \param me
-  /// \return
+  /// This returns the amount of tics that have passed on a timer linked to an identification string
+  /// since the first time this function was called (this function creates and associates a timer
+  /// with the id string if such a timer thus not already exit).
+  ///
+  /// \param me This is the identification string of the timer
+  /// \return The amount of ticks since timer created
   int getTime(const std::string& me);
+  /// \brief Check weather an identification string has timer
   ///
-  /// \param me
-  /// \return
+  /// This function checks weather the passed in identification string has a timer associated with it
+  /// or not. Returns true if it does.
+  ///
+  /// \param me This is the identification string of the timer
+  /// \return True if there is a timer associated with the identification string.
   bool hasTime(const std::string& me);
+  /// \brief Removes timer associated with identification string.
   ///
-  /// \param me
+  ///  This function removes the timer associated with the identification string from the singleton.
+  ///
+  /// \param me This is the identification string of the timer
   void removeTime(const std::string& me);
+  /// \brief Lets the clock tick
   ///
+  /// Make the clock tick by incrementing the value of each timer in the singleton but one.
   void tick();
 protected:
-  ///
+  /// Constructor blocked from being accessed by rest of system.
   Clock();
-  ///
+  /// Copy constructor blocked from being accessed by rest of system.
   /// \param clock
   Clock(const Clock& clock);
-  ///
+  /// Assigment operator blocked from being accessed by rest of system.
   /// \return
   Clock& operator=(const Clock&);
-  ///
+  /// Destructor operator blocked from being accessed by rest of system.
   ~Clock();
 };
 
