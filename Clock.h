7affdfae2d373d4cea95eddc8cf4e1f52bb5c6bb
Simon <u22490729@tuks.co.za>
2023-10-26 10:07:41 +0200

    Added file headers for Clock.cpp and Clock.h

--- Clock.h
+++ Clock.h
@@ -1,6 +1,15 @@
+//===-- COS214_Project/Clock.h - Clock class definition ------*- C++ -*-===//
 //
-// Created by maili on 2023/10/26.
+// COS 214 Project.
 //
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the declaration of an Clock class. The Clock is a
+/// singleton that will be used through out the system the synchronise a
+/// actions between classes.
+///
+//===----------------------------------------------------------------------===//
 
 #ifndef COS214_PROJECT__CLOCK_H
 #define COS214_PROJECT__CLOCK_H
