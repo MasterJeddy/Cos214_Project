afb47be4da94ca99b2c07f267decf868c989758e
Simon <u22490729@tuks.co.za>
2023-10-26 09:43:33 +0200

    Added file headers for SavedLog.cpp and SavedLog.h

--- SavedLog.cpp
+++ SavedLog.cpp
@@ -1,5 +1,14 @@
+//===-- COS214_Project/SavedLog.cpp - SavedLog class implementation ------*- C++ -*-===//
 //
-// Created by maili on 2023/10/25.
+// COS 214 Project.
 //
+//===----------------------------------------------------------------------===/////
+/// \file
+/// This file contains the implementation of the SavedLog class. SavedLog serves
+/// as the memento in the memento design. It stores the same same vector of
+/// commands as the CommandLog class. It can be save and loaded with the uses
+/// of the Logs Class.
+///
+//===----------------------------------------------------------------------===//
 
 #include "SavedLog.h"
--- SavedLog.h
+++ SavedLog.h
@@ -1,6 +1,16 @@
+//===-- COS214_Project/SavedLog.h - SavedLog class definition ------*- C++ -*-===//
 //
-// Created by maili on 2023/10/25.
+// COS 214 Project.
 //
+//===----------------------------------------------------------------------===/////
+/// \file
+/// This file contains the declaration of the SavedLog class. SavedLog serves
+/// as the memento in the memento design. It stores the same same vector of
+/// commands as the CommandLog class. It can be save and loaded with the uses
+/// of the Logs Class.
+///
+//===----------------------------------------------------------------------===//
+
 
 #ifndef COS214_PROJECT__SAVEDLOG_H
 #define COS214_PROJECT__SAVEDLOG_H
