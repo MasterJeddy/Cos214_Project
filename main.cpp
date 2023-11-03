c9b3df844b294fdd386fda704dc61834e2674414
Simon <u22490729@tuks.co.za>
2023-11-03 12:37:09 +0200

    some changes

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 //
 // Created by maili on 2023/10/23.
 //
+#include "IOInterface.h"
 
 int randomvalue(){
   return 3;
 }
 
 int main() {
-
+  auto* io = new IOInterface();
+  io->poll();
+  delete io;
   return 0;
 }
